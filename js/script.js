27736b7a77a0586b0c1f1799767f1a76734ab6e0
Showmmoy Barua <128532264+Showmmoy@users.noreply.github.com>
2025-08-02 06:22:58 +0100

    addeded mix it plugin and make them responsive

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// js
+// Recent work
+ var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
+
+            var mixer = mixitup(containerEl, {
+                selectors: {
+                    target: '[data-ref~="mixitup-target"]'
+                }
+            });
+
+// jQuery
+$(document).ready(function() {
+  
+});
